e53dff3af05c19f45dd6663a3b5d9913295640d6
Riddhi K <riddhik84@gmail.com>
2020-06-24 16:32:02 +0530

    Updated with better solution

--- String/Leedcode/LongestCommonPrefix.java
+++ String/Leedcode/LongestCommonPrefix.java
@@ -1,6 +1,28 @@
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/887/
 
 class LongestCommonPrefix {
+    //Easy and Fast solution - this must be used
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+        for(int i = 0; i < strs[0].length(); i++){
+            
+            //Take first char of first string in the list as prefix by default
+            String prefix = strs[0].substring(0, i+1);
+            
+            //check the current prefix in all other strings in the list
+            for(int j= 1; j < strs.length; j++) {
+                if(!strs[j].startsWith(prefix)) { //need to use String class method startsWith()
+                    //If the prefi do not match with any of the strings that means prefix - (prefixlen - 1) is match for all strings
+                    //so return that as prefix
+                    return prefix.substring(0, prefix.length()-1);
+                }
+            }
+        }
+        return strs[0];
+    }
+    
+    //Good solution but bit slow to execute
     public String longestCommonPrefix(String[] strs) {
         String prefix = "";
         //check for null and empty array
