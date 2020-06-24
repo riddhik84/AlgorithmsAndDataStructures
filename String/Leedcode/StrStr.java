c76188bb15e7b880adef5ecb0814669f2a80321e
Riddhi K <riddhik84@gmail.com>
2020-06-24 15:40:06 +0530

    Updated with explanation of algo

--- String/Leedcode/StrStr.java
+++ String/Leedcode/StrStr.java
@@ -1,23 +1,24 @@
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/885/
 class StrStr {
     public int strStr(String haystack, String needle) {
-       int index = -1;
+      int index = -1;
         if (needle == "" || needle.length() == 0 || haystack.equals(needle)) {
             return 0;
         }
-        if (haystack.length() > needle.length()) {
+        if (haystack.length() > needle.length()) { //check this condition to start algo
             for (int i = 0; i < haystack.length(); i++) {
-                if (haystack.charAt(i) == needle.charAt(0)) {
+                if (haystack.charAt(i) == needle.charAt(0)) { //start needle loop with first char is found
                     index = i;
-                    int k = i;
+                    int k = i; //need k to loop haystack keep i of outer loop as is
                     for (int j = 0; j < needle.length(); j++, k++) {
-                        if ((k == haystack.length()) || (!(haystack.charAt(k) == needle.charAt(j)))) {
+                        // look for below conditions
+                        if ((k == haystack.length()) || (haystack.charAt(k) != needle.charAt(j))) {
                             index = -1;
                             break;
                         }
                     }
                 }
-                if (index != -1) {
+                if (index != -1) { //needle is found index will not be -1
                     break;
                 }
             }
