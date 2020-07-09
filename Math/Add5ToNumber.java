d4ef9faba10571096e499e4029709f4b468dd788
RKs <riddhik84@gmail.com>
2020-07-09 19:40:26 +0530

    Codility code

---
+++ Math/Add5ToNumber.java
@@ -0,0 +1,14 @@
+
+public class Add5ToNumber {
+	public static void main(String[] args){
+		//range -8000 8000
+		int[] input = {};
+		for(int i : input){
+			System.out.println("i: " + i + " " + maxNumber(i));
+		}
+	}
+	
+	public static int maxNumber(int N){
+		
+	}
+}
