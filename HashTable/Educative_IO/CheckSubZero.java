efdc53480276dd2e3ff0555dedb5f3f047404dfd
riddhik84 <riddhik84@gmail.com>
2020-12-28 09:48:00 +0530

    Solutions

---
+++ HashTable/Educative_IO/CheckSubZero.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class CheckSubZero {
+
+  public static boolean findSubZero(int[] arr) {
+
+    //Use HashMap to store Sum as key and index i as value till sum has been calculated.
+    //Traverse the array and return true if either 
+    //arr[i] == 0 or sum == 0 or HashMap already contains the sum
+    //If you completely traverse the array and havent found any of the above three
+    //conditions then simply return false.
+    HashMap <Integer, Integer> hMap = new HashMap < >();
+    int sum = 0;
+
+    // Traverse through the given array
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+
+      if (arr[i] == 0 || sum == 0 || hMap.get(sum) != null) 
+		return true;
+
+      hMap.put(sum, i);
+    }
+
+    return false;
+  }
+
+  public static void main(String args[]) {
+
+    int[] arr1 = {6, 4, -7, 3, 12, 9};
+    System.out.println(findSubZero(arr1));
+	
+	int[] arr2 = {6, 4, 7, 3, 12, 9};
+    System.out.println(findSubZero(arr2));
+
+  }
+}
---
+++ HashTable/Educative_IO/TwoPairsInArray.java
@@ -0,0 +1,30 @@
+//output: {4,12}{7,9}
+
+import java.util.*;
+
+public class TwoPairsInArray {
+	
+	public static void main(String[] args){
+		int[] arr = {3, 4, 7, 1, 12, 9};
+		findPair(arr);
+	}
+	
+	public static void findPair(int[] arr){
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		for(int i = 0; i < arr.length; i++){
+			for(int j = i + 1; j < arr.length; j++){
+				int sum = arr[i] + arr[j];
+				
+				if(map.containsKey(sum)){
+					System.out.println(map.get(sum) + "{" + arr[i] + "," + arr[j] + "}");
+					return;
+				} else {
+					String value = "{" + arr[i] + "," + arr[j] + "}";
+					map.put(sum, value);
+				}
+			}
+		}
+		System.out.println("No match found");
+	}
+}
